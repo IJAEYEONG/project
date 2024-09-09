eca0bb2a9a39a65cc14841d0e701c46e4355dc91
leejeayoung <leejeayoung875@gmail.com>
2024-09-09 11:29:35 +0900

    list,dict 사용

--- testOne.py
+++ testOne.py
@@ -2,6 +2,20 @@ a="test"
 b="t"
 
 def stringMerge():
+    global a,b 
     return a+b;
 
 print(stringMerge())
+
+
+def example():
+    return "예제"
+
+anther_example=lambda:"예제"
+
+array =["배성빈","문혜림","신지윤"]
+obj ={
+    "조자연":"안녕",
+    "김보미":"개굴",
+    "황재민":"수면왕"
+}
